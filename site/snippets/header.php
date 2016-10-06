de29ca9cbe541e14fcc6a94a116947061b4aca93
emmo48 <emmo48@gmail.com>
2016-10-06 12:35:39 -0700

    Add files via upload

---
+++ site/snippets/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <link href="http://myipadfix.com/favicon.ico" rel="icon" />
+  <meta name="description" content="<?php echo $site->description()->html() ?>">
+  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+
+  <?php echo css('assets/css/myipadfix.css') ?>
+
+</head>
+
+<body>
+
+  <header class="headerimage">
+
+    <a href="http://myipadfix.com">
+      <img alt="Main Menu" src="/assets/images/hillbrooklogo.png" style="float: left;
+  margin-left: 25px;" /></a> 
+    
+  </header>
---
+++ site/templates/home.php
@@ -0,0 +1,47 @@
+
+<div class="titlehidden">
+
+<?php snippet('header') ?>
+
+</div>
+
+<body>
+
+  <div id="wrapper">
+
+    <div style="text-align: center">
+
+      <?php echo $page->text()->kirbytext() ?>
+
+      </div>
+
+    <div class="menucategory" style="top: 33%; left: 33%;"> 
+    <a href="http://www.myipadfix.com/power"><img src="assets/images/power@2x.png" width="100px"/></a>
+    <div class="caption">Power</div>
+    </div>
+    
+    <div class="menucategory" style="top: 38%; left: 55.5%;">
+    <a href="http://www.myipadfix.com/wifi"><img src="assets/images/Network@2x.png" width="75px"/></a>
+    <div class="caption">WiFi</div>
+    </div>
+
+    <div class="menucategory" style="top: 75%; left: 22%;">
+    <a href="http://www.myipadfix.com/hardware"><img src="assets/images/hardware@2x.png" width="85px"/></a>
+    <div class="caption">Hardware</div>
+    </div>
+
+    <div class="menucategory" style="top: 70%; left: 45%;">
+    <a href="http://www.myipadfix.com/accounts"><img src="assets/images/Accounts2@2x.jpg" width="75px"/></a>
+    <div class="caption">Accounts</div>
+    </div>
+
+    <div class="menucategory" style="top: 71.5%; left: 68%;">
+    <a href="http://www.myipadfix.com/software"><img src="assets/images/Apps.png" width="85px"/></a>
+    <div class="caption">Software</div>
+    </div>
+
+  </div>
+
+  
+
+<?php snippet('footer') ?> 
